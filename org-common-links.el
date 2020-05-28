9d9f5e72c13891fc2b1c8a6137d017406799fddf
leejiahong <jia_hong@live.com.my>
2020-05-28 22:06:05 +0800

    Use org-link-set-parameters instead of org-add-link-type

--- org-common-links.el
+++ org-common-links.el
@@ -6,37 +6,52 @@
   :prefix "org-common-link-")
 
 ;;; Wikipedia
-(org-add-link-type "wikipedia" 'org-common-links--open-wikipedia)
-
 (defcustom org-common-links-wikipedia-url "https://www.wikipedia.org/wiki/%s"
   "The wikipedia url to open topic."
   :group 'org-common-link
   :type '(string))
 
-(defun org-common-links--open-wikipedia (link)
+(defun org-common-links--open-wikipedia-link (link)
   "Open the given wikipedia org-link."
-  (let ((finalurl (format org-common-links-wikipedia-url link)))
-    (browse-url finalurl)))
+  (browse-url (format org-common-links-wikipedia-url link)))
 
+(defun org-common-links--export-wikipedia-link (link description format)
+  "Export the wikipedia LINK with DESCRIPTION for FORMAT from Org files."
+  (let ((fullurl (format org-common-links-wikipedia-url link)))
+    (cond ((eq format 'html)
+           (format "<a href=\"%s\">%s</a>" fullurl description))
+          ((eq format 'latex)
+           (format "\\href{%s}{%s}" fullurl description))
+          ((eq format 'ascii)
+           (format "%s (%s)" fullurl description))
+          (t esplink))))
+
+(org-link-set-parameters "wikipedia"
+                         :follow #'org-common-links--open-wikipedia-link
+                         :export #'org-common-links--export-wikipedia-link)
 ;;; MAN
-(org-add-link-type "man" 'org-man-open)
-(add-hook 'org-store-link-functions 'org-man-store-link)
-
-(defcustom org-man-command 'man
+(defcustom org-common-links-man-command 'man
   "The Emacs command to be used to display a man page."
   :group 'org-link
   :type '(choice (const man) (const woman)))
 
-(defun org-man-open (path)
+(defun org-common-links--open-man (path)
   "Visit the manpage on PATH.
 PATH should be a topic that can be thrown at the man command."
-  (funcall org-man-command path))
+  (funcall org-common-links-man-command path))
 
-(defun org-man-store-link ()
+(defun org-common-links--man-get-page-name ()
+  "Extract the page name from the buffer name."
+  ;; This works for both `Man-mode' and `Woman-mode'.
+  (if (string-match " \\(\\S-+\\)\\*" (buffer-name))
+      (match-string 1 (buffer-name))
+    (error "Cannot create link to this man page.")))
+
+(defun org-common-links--store-man-link ()
   "Store a link to a manpage."
   (when (memq major-mode '(Man-mode woman-mode))
     ;; This is a man page, make this link
-    (let* ((page (org-man-get-page-name))
+    (let* ((page (org-common-links--man-get-page-name))
            (link (concat "man:" page))
            (description (format "Manpage for %s" page)))
       (org-store-link-props
@@ -44,36 +59,65 @@ PATH should be a topic that can be thrown at the man command."
        :link link
        :description description))))
 
-(defun org-man-get-page-man ()
-  "Extract the page name from the buffer name."
-  ;; This works for both `Man-mode' and `Woman-mode'.
-  (if (string-match " \\(\\S-+\\)\\*" (buffer-name))
-      (match-string 1 (buffer-name))
-    (error "Cannot create link to this man page.")))
+(org-link-set-parameters "man"
+                         :follow #'org-common-links--open-man
+                         :store #'org-common-links--store-man-link)
 
 ;;; PEP
-(org-add-link-type "pep" 'org-pep-open)
-
-(defun org-pep-open (number)
+(defun org-common-links--open-pep (number)
   "Visit pep NUMBER on python.org"
   (let* ((base-string "https://www.python.org/dev/peps/pep-%04d/")
          (num (string-to-number number))
          (final-url (format base-string num)))
     (browse-url final-url)))
 
-;;; GITHUB
-(org-add-link-type "github" 'org-github-open)
+(defun org-common-links--export-pep (link description format)
+  "Export the pep LINK with DESCRIPTION for FORMAT from Org files."
+  (let* ((base-string "https://www.python.org/dev/peps/pep-%04d/")
+         (num (string-to-number number))
+         (fullurl (format base-string num)))
+    (cond ((eq format 'html)
+           (format "<a href=\"%s\">%s</a>" fullurl description))
+          ((eq format 'latex)
+           (format "\\href{%s}{%s}" fullurl description))
+          ((eq format 'ascii)
+           (format "%s (%s)" fullurl description))
+          (t fullurl))))
 
-(defun org-github-open (REPO)
-  "Visit REPO at GitHub.com."
+(org-link-set-parameters "pep"
+                         :follow #'org-common-links--open-pep
+                         :export #'org-common-links--export-pep)
+
+;;; GITHUB
+(defun org-common-links--format-github-link (link)
+  "Format PATH into a complete url."
   (let* ((base-url "https://github.com/%s")
          (issue-url "/issues/%s")
-         (hashpos (string-match-p "#" REPO)))
+         (hashpos (string-match-p "#" link)))
     (if hashpos
-        (browse-url
-         (concat (format base-url (substring REPO nil hashpos))
-                 (format issue-url (substring REPO (+ hashpos 1) nil)))) ;; extract without '#'
-      (browse-url (format base-url REPO)))))
+        (concat (format base-url (substring link nil hashpos))
+                (format issue-url (substring link (+ hashpos 1) nil))) ;; extract without '#'
+      (format base-url link))))
+
+(defun org-common-links--open-github-link (link)
+  "Visit link at GitHub.com."
+  (browse-url (org-common-links--format-github-link link)))
+
+(defun org-common-links--export-github-link (link description format)
+  "Export the github LINK with DESCRIPTION for FORMAT from Org files."
+  (let* ((fullurl (org-common-links--format-github-link link))
+         (desc (or description fullurl)))
+    (cond ((eq format 'html)
+           (format "<a href=\"%s\">%s</a>" fullurl desc))
+          ((eq format 'latex)
+           (format "\\href{%s}{%s}" fullurl desc))
+          ((eq format 'ascii)
+           (format "%s (%s)" fullurl desc))
+          (t fullurl))))
+
+(org-link-set-parameters "github"
+                         :follow #'org-common-links--open-github-link
+                         :export #'org-common-links--export-github-link)
 
 (provide 'org-common-links)
 
